4f32ec557e5749a5e769e3dc50b0a08a2c310b5c
ChinhTC <assin2910@gmail.com>
2025-06-17 12:13:19 +0700

    feat: add grpc repository of product option

--- GrpcServiceProduct/Interfaces/IProductOptionRepository.cs
+++ GrpcServiceProduct/Interfaces/IProductOptionRepository.cs
@@ -5,9 +5,9 @@ namespace GrpcServiceProduct.Interfaces
 {
     public interface IProductOptionRepository
     {
-        Task<IEnumerable<Domain.Entities.ProductOption>> GetAll();
-        Task<IEnumerable<Domain.Entities.ProductOption>> GetAllOfProduct(string productId);
-        Task<Domain.Entities.ProductOption?> GetOne(string optionId);
+        Task<IEnumerable<ResponseProductOption>> GetAll();
+        Task<IEnumerable<ResponseProductOption>> GetAllOfProduct(string productId);
+        Task<ResponseProductOption?> GetOne(string optionId);
         Task<Response> CreateProductOption(RequestCreateOption createOption);
         Task<Response> CreateManyProductOption(RequestCreateOption[] createOption);
         Task<Response> UpdateProductOption(RequestUpdateOption updateOption);
