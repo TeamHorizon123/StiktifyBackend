59001f9cbc4371596d906cf8cb47529fd77b16c5
ChinhTC <assin2910@gmail.com>
2025-06-20 15:28:42 +0700

    base: config grpc service order

--- GrpcServiceOrder/Program.cs
+++ GrpcServiceOrder/Program.cs
@@ -1,14 +1,17 @@
-using GrpcServiceOrder.Services;
+using GrpcServiceOrder.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(option
+    => option.UseNpgsql(builder.Configuration["ConnectionStrings:Db"]));
 builder.Services.AddGrpc();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+//app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
