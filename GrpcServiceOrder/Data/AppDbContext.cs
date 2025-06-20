3fb385097d3a83407cb9ba9bf9a76372f84e0292
ChinhTC <assin2910@gmail.com>
2025-06-20 18:13:24 +0700

    config docker for grpc service order

--- GrpcServiceOrder/Data/AppDbContext.cs
+++ GrpcServiceOrder/Data/AppDbContext.cs
@@ -5,5 +5,15 @@ namespace GrpcServiceOrder.Data
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Domain.Entities.Cart> Carts { get; set; }
+        public DbSet<Domain.Entities.Order> Orders { get; set; }
+        public DbSet<Domain.Entities.OrderDetail> OrderDetails { get; set; }
+        public DbSet<Domain.Entities.OrderTracking> OrderTrackings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
