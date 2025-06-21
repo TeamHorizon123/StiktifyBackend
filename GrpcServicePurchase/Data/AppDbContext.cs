7c4393d0f158f02beaf3b7eb98180a369cf8647c
ChinhTC <assin2910@gmail.com>
2025-06-21 13:29:23 +0700

    Config Grpc Service Purchase

---
+++ GrpcServicePurchase/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GrpcServicePurchase.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Domain.Entities.Payment> Payments { get; set; }
+        public DbSet<Domain.Entities.PaymentMethod> PaymentMethods { get; set; }
+        public DbSet<Domain.Entities.PaymentRefund> PaymentRefunds { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Domain.Entities.Payment>().ToTable("Payment");
+            modelBuilder.Entity<Domain.Entities.PaymentMethod>().ToTable("PaymentMethod");
+            modelBuilder.Entity<Domain.Entities.PaymentRefund>().ToTable("PaymentRefund");
+        }
+    }
+}
--- GrpcServicePurchase/Program.cs
+++ GrpcServicePurchase/Program.cs
@@ -1,14 +1,21 @@
+using GrpcServicePurchase.Data;
+using GrpcServicePurchase.Interfaces;
 using GrpcServicePurchase.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(option
+    => option.UseNpgsql(builder.Configuration["ConnectionStrings:Db"]));
 builder.Services.AddGrpc();
+
+builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+app.MapGrpcService<PaymentGrpcService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
